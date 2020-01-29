07ee872c1e5708a79377af54b40ec89679b2b683
havina.ai <havina.ai@gutagroup.ru>
2020-01-29 14:47:09 +0300

    сделала задания 3 модуля

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -5,11 +5,11 @@
  */
 function sumSalary(salaries) {
   let sum = 0;
-  for ( key in salaries) {
+  for (key in salaries) {
 
     if(typeof salaries[key]  == 'number') {
      sum += salaries[key];
     }
-
   }
-  return sum;}
+  return sum;
+}
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,4 +4,8 @@
  * @returns {Boolean}
  */
 function isEmpty(obj) {
+  for(let key in obj) {
+    return false;
+  }
+  return true;
 }
